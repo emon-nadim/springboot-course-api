65c5e48bacf5c845272d32b0407c9eea1d3ff80a
Nadim Iqbal <nadim.iqbal@ericsson.com>
2020-03-24 23:24:43 +0600

    addiing initial project source

---
+++ src/main/java/com/nadimemon/springbootcourseapi/SpringbootCourseApiApplication.java
@@ -0,0 +1,13 @@
+package com.nadimemon.springbootcourseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCourseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCourseApiApplication.class, args);
+	}
+
+}
